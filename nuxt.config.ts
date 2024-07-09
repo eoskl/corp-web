aa54e6a96669a1b7a3722ff26a9675f393da3516
eoskl <consult@vpnet.de>
2024-07-09 20:13:51 +0800

    Fix error handling

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,12 @@
 export default defineNuxtConfig({
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
   ssr: false,
+
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
+
   devtools: { enabled: true },
+
   modules: [
     "@ant-design-vue/nuxt",
     "@nuxt/eslint",
@@ -17,17 +20,21 @@ export default defineNuxtConfig({
     "@nuxtjs/snipcart",
     "@nuxtjs/tailwindcss"
   ],
+
   snipcart: {
     templatesUrl: '/snipcart-custom.html',
     publicApiKey: "NjFlNzA3NjktZGQwNC00Yjg0LWIwY2QtMjdmNmI0MmI5Y2MyNjM4NTUyNjY2MDgyOTExMzEx"
   },
+
   plugins: [
     { src: '~/plugins/main-style.js', mode: 'client' },
     { src: '~/plugins/useSnipcart.js', mode: 'client' }
   ],
+
   css: [
     '~/assets/stylesheets/main/tailwind.css',
   ],
+
   build: {
     postcss: {
       postcssOptions: {
@@ -38,4 +45,6 @@ export default defineNuxtConfig({
       },
     },
   },
+
+  compatibilityDate: "2024-07-08",
 })
--- pages/products.vue
+++ pages/products.vue
@@ -1,23 +1,27 @@
 <template>
-    <div>
-      <p v-for="user in users" :key="user.id">ID: {{ user.id }} 👉 {{ user.name }}</p>
-    </div>
-  </template>
-  
-  
-  <script>
-  definePageMeta({
-    layout: "products"
-  })
-  
-  export default {
-    data () {
-      return {
-        users: '',
-      }
-    },
-    async mounted() {
-      this.users = await $fetch('https://jsonplaceholder.typicode.com/users')
-    },
+  <div>
+    <p v-for="user in users" :key="user.id">ID: {{ user.id }} 👉 {{ user.name }}</p>
+  </div>
+</template>
+
+<script>
+definePageMeta({
+  layout: "products"
+});
+
+export default {
+  data() {
+    return {
+      users: []
+    };
+  },
+  async mounted() {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+      this.users = await response.json();
+    } catch (error) {
+      console.error('Error fetching users:', error);
+    }
   }
-  </script>
+};
+</script>
